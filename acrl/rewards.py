21105249cb298d03d68b77c20bab89e056e67565
SamuelOrtizV <86854548+SamuelOrtizV@users.noreply.github.com>
2025-04-27 11:58:50 -0400

    Update rewards.py

--- acrl/rewards.py
+++ acrl/rewards.py
@@ -115,10 +115,10 @@ class RewardFunction:
             reward += self.penalty_low_rpms/2
         elif telemetry_data["rpms"] < self.threshold_rpms:
             reward += self.penalty_low_rpms
-        if telemetry_data["speed"] < 1.0:  # If the car is not moving
+        if telemetry_data["speed"] < self.threshold_speed:  # If the car is not moving
             reward += self.penalty_low_speed*50
             mistake = True 
-        elif telemetry_data["speed"] < self.threshold_speed:
+        elif telemetry_data["speed"] < self.threshold_speed*2:
             reward += self.penalty_low_speed        
         if position_difference < 0:
             reward += self.penalty_backwards
