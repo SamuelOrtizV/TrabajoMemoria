9395a5ba95bf4574d4ecee4d5e22cf75ff33f9df
SamuelOrtizV <86854548+SamuelOrtizV@users.noreply.github.com>
2025-05-07 20:31:21 -0400

    Update replay_buffer_reset.py

--- acrl/replay_buffer_reset.py
+++ acrl/replay_buffer_reset.py
@@ -1,4 +1,4 @@
-from tmrl.util import load
+from tmrl.util import load, dump
 import tmrl.config.config_constants as cfg
 import random
 import matplotlib.pyplot as plt
@@ -10,6 +10,13 @@ checkpoint = load(checkpoint_path)
 replay_memory = checkpoint.memory
 
 try:
+    checkpoint.memory = []
+    dump(checkpoint, checkpoint_path)
+    print("Replay Buffer vaciado correctamente\n")
+except Exception as e:
+    print(f"No se pudo limpiar el Replay Buffer\n {e}")
+
+""" try:
     while True:
         # Seleccionar una imagen aleatoria del buffer
         if len(replay_memory.data[3]) > 0:  # Asegúrate de que haya imágenes en el buffer
@@ -24,4 +31,4 @@ try:
         else:
             print("No hay imágenes en el buffer para mostrar.")
 except KeyboardInterrupt:
-    print("Interrumpido por el usuario. Saliendo...")
+    print("Interrumpido por el usuario. Saliendo...") """
